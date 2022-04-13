de299a45fb29852abec75a3b73816313791c888e
Anti20-creator <b264lke@gmail.com>
2022-04-13 13:49:09 +0200

    Invoice generating is working with more pages

---
+++ utils/tester.js
@@ -0,0 +1,206 @@
+const { createInvoice } = require('./InvoiceCreator')
+
+createInvoice([
+    {
+        price: 1200,
+        quantity: 1,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 2,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 3,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 4,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 5,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 6,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 7,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 8,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 9,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 10,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 11,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 12,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 13,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 14,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 15,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 16,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 17,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 18,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 19,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 20,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 1,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 2,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 3,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 4,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 5,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 6,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 7,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 8,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 9,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 10,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 11,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 12,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 13,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 14,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 15,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 16,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+    {
+        price: 1200,
+        quantity: 17,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 18,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 19,
+        name: 'Charolais marhatatár'
+    },
+    {
+        price: 1200,
+        quantity: 20,
+        name: 'Ragacsos mangalicaoldalas, fűszeres házi chips, körtés káposztasaláta'
+    },
+], 'random.pdf', "123456789123458", "62569ba8e54de20b2fc3ea49", "amtmannkristof@gmail.com", () => {
+    console.log('done')
+})
